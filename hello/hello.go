a368655cba1ae87b5c98b835279392e8b40653b2
Alvin <ses8104@naver.com>
2023-05-20 11:43:25 +0900

    first commit

---
+++ hello/hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Hello World")
+	var a int = 10
+	var msg string = "Hello Variable"
+
+	msg1 := "Alvin"
+
+	a = 20
+	msg = "Good Moring"
+	fmt.Println(msg, msg1, a)
+
+}
